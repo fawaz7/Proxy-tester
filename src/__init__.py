8dac6546495f6ee7d70f5abb04000739512f4399
Fawaz <ghazawe1@gmail.com>
2025-10-27 13:38:51 +0300

    Bump version to 1.1.1

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ Fawaz: Proxy Tester (Fwz_PT)
 A multi-threaded proxy testing tool for HTTP/SOCKS5 proxies
 """
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __author__ = "Fawaz Al-Ghzawi"
 __email__ = "Ghazawe1@gmail.com"
 __description__ = "A multi-threaded proxy testing tool for HTTP/SOCKS5 proxies"
